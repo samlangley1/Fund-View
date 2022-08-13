ce8a670161a94a91c51a017cc55290d0d602823d
samlangley1 <samlangley89@hotmail.com>
2022-08-13 09:52:20 +0100

    general improvements

--- backend/fetcher.py
+++ backend/fetcher.py
@@ -1,4 +1,4 @@
-from backend.params import fetch_url,api_key
+from backend.params import fetch_url,api_key,subscription_plan,alert_only
 #from backend.paramsdev import api_key
 from backend.processor import Processor
 import requests
@@ -41,26 +41,34 @@ class Fetcher():
         for index in range(len(data)):
             try:
                 for i in (data[index]):
-                    stock = data[index]["shortName"]
-                    stock_price = data[index]["regularMarketPrice"]
-                    stock_eps_current = data[index]["epsCurrentYear"]
-                    stock_eps_forward = data[index]["epsForward"]
-                    stock_eps_trailing = data[index]["epsTrailingTwelveMonths"]
-                    stock_forward_price_earnings = data[index]["forwardPE"]
-                    stock_200_day_avg_change_percent = data[index]["twoHundredDayAverageChangePercent"]
-                    stock_pe_ratio = (stock_price / stock_eps_trailing)
-                    stock_forward_pe_ratio = (stock_price / stock_eps_current)
-                    stock_eps_percent_change = ((stock_eps_forward - stock_eps_current) / (
-                            (stock_eps_forward + stock_eps_current) / 2)) * 100
-                    stock_peg_ratio = stock_pe_ratio / stock_eps_percent_change
+                        stock = data[index]["shortName"]
+                        stock_price = data[index]["regularMarketPrice"]
+                        stock_eps_current = data[index]["epsCurrentYear"]
+                        stock_eps_forward = data[index]["epsForward"]
+                        stock_eps_trailing = data[index]["epsTrailingTwelveMonths"]
+                        stock_forward_price_earnings = data[index]["forwardPE"]
+                        stock_200_day_avg_change_percent = data[index]["twoHundredDayAverageChangePercent"]
+                        stock_pe_ratio = (stock_price / stock_eps_trailing)
+                        stock_forward_pe_ratio = (stock_price / stock_eps_current)
+                        stock_eps_percent_change = ((stock_eps_forward - stock_eps_current) / (
+                                (stock_eps_forward + stock_eps_current) / 2)) * 100
+                        stock_peg_ratio = stock_pe_ratio / stock_eps_percent_change
 
+
+                if alert_only == True:
                 # EXAMPLE CONDITION
-                # if 0 < stock_peg_ratio < 2 and 0 < stock_pe_ratio < 5 and stock_eps_percent_change > 10:
-                print(
-                        f"{stock} is currently at {stock_price} per share.\nCurrent EPS {stock_eps_current} -1y:"
-                        f" {stock_eps_trailing} +1y: {stock_eps_forward} +1y % change prediction: "
-                        f"{stock_eps_percent_change}\nP/E Ratio: {stock_pe_ratio} P"
-                        f"EG Ratio: {stock_peg_ratio}\n\n\n\n")
+                    if 0 < stock_peg_ratio < 2 and 0 < stock_pe_ratio < 5 and stock_eps_percent_change > 10:
+                        print(
+                                f"{stock} is currently at {stock_price} per share.\nCurrent EPS {stock_eps_current} -1y:"
+                                f" {stock_eps_trailing} +1y: {stock_eps_forward} +1y % change prediction: "
+                                f"{stock_eps_percent_change}\nP/E Ratio: {stock_pe_ratio} P"
+                                f"EG Ratio: {stock_peg_ratio}\n\n\n\n")
+                else:
+                    print(
+                                f"{stock} is currently at {stock_price} per share.\nCurrent EPS {stock_eps_current} -1y:"
+                                f" {stock_eps_trailing} +1y: {stock_eps_forward} +1y % change prediction: "
+                                f"{stock_eps_percent_change}\nP/E Ratio: {stock_pe_ratio} P"
+                                f"EG Ratio: {stock_peg_ratio}\n\n\n\n")
             except:
                 pass
 
--- backend/params.py
+++ backend/params.py
@@ -1,2 +1,9 @@
 fetch_url = "https://yfapi.net/v6/finance/quote"
 api_key = "YOUR_API_KEY"
+
+# The plan that your API key relates to (Choose one of "BASIC", "PRO", "ULTRA", "MEGA")
+subscription_plan = "BASIC"
+
+
+# False will display all scraped stocks, True will only show scraped stocks that meet the criteria defined in fetcher.py
+alert_only = False
